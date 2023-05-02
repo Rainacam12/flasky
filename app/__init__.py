ac9718ad25abe52e67fc61614d439aaf5122b6a7
Rainacam <rainacam12@gmail.com>
2023-05-02 09:53:37 -0400

    more connect steps

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,30 @@
 from flask import Flask
+# 4/28 import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+# import Migrate 
+from flask_migrate import Migrate
+
+# give us access to db ops
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+    
+    # set up the database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
+
+    # connect the db and migrate to our flask app
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # import routes
     from .routes import crystal_bp
     # register the blueprint
     app.register_blueprint(crystal_bp)
     
+    # import so db can see the model
+    from app.models.crystal import Crystal
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,69 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.crystal import Crystal
 
-class Crystal:
-    def __init__(self, id, name, color, powers):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.powers = powers
+# 4/28 comment out crystal class and hardcoded data on line 11
+# class Crystal:
+#     def __init__(self, id, name, color, powers):
+#         self.id = id
+#         self.name = name
+#         self.color = color
+#         self.powers = powers
 
 # create a list of instances
-crystals = [
-    Crystal(1, "Amethyst", "Purple", "Infinite knowledge and wisdom"),
-    Crystal(2, "Tiger's Eye", "Gold", "Confidence, strength"),
-    Crystal(3, "Rose Quartz", "Pink", "Love")
-]
+# crystals = [
+#     Crystal(1, "Amethyst", "Purple", "Infinite knowledge and wisdom"),
+#     Crystal(2, "Tiger's Eye", "Gold", "Confidence, strength"),
+#     Crystal(3, "Rose Quartz", "Pink", "Love")
+# ]
 
 crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
 # create endpoint to get resources
-@crystal_bp.route("", methods=["GET"])
+# @crystal_bp.route("", methods=["GET"])
 
+# def handle_crystals():
+#     crystal_response = []
+#     for crystal in crystals:
+#         crystal_response.append({
+#             "id": crystal.id,
+#             "name": crystal.name,
+#             "color": crystal.color,
+#             "powers": crystal.powers
+#         })
+
+#     return jsonify(crystal_response)
+
+@crystal_bp.route("", methods=['POST'])
+
+# define route for creating a crystal
 def handle_crystals():
-    crystal_response = []
+    request_body = request.get_json()
+
+    new_crystal = Crystal(
+        name = request_body["name"],
+        color = request_body["color"],
+        powers = request_body["powers"]
+    )
+
+    db.session.add(new_crystal)
+    db.session.commit()
+
+    return make_response(f"Crystal {new_crystal.name} successfully created", 201)
+
+# define a route for getting all crystals 
+@crystal_bp.route("", methods=['GET'])
+def read_all_crystals():
+    crystals_response = []
+    # query.all gets crystals from db
+    crystals = Crystal.query.all()
+
+    # add each crystal to the response body
     for crystal in crystals:
-        crystal_response.append({
+        crystals_response.append({
             "id": crystal.id,
             "name": crystal.name,
             "color": crystal.color,
             "powers": crystal.powers
         })
-
-    return jsonify(crystal_response)
+    return jsonify(crystals_response)
